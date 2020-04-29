0997b5a658f6e7de63d988e18c13da0ccb457d4e
YJoye <18292307391@163.com>
2020-04-29 12:51:33 +0800

    gc

--- 01-base/17-class2.py
+++ 01-base/17-class2.py
@@ -24,3 +24,41 @@ d1.say_hello()
 
 d1.set_name("xiaohei")
 d1.say_hello()
+
+print("*"*40)
+
+class Cat() :
+
+	# 使用装饰器@property来定义getter和setter的话
+	# 需要用 下划线形式 的属性
+	def __init__(self, name):
+		self._name = name
+
+	# getter方法通过属性的方式来实现
+	@property
+	def name(self) :
+		return self._name
+
+	# setter方法通过属性的方式来实现
+	@name.setter
+	def name(self, name) :
+		self._name = name
+
+	
+c1 = Cat("芒果")
+
+# getter
+print(c1.name)
+
+c1.name = "坚果"
+print(c1.name)
+
+
+# 通过super()返回的对象调用父类的方法的时候不需要self这个参数
+# super().__init__(self, name)
+
+# 类名.__bases__ 获取当前类的所有父类
+
+# 多重继承 : a同时继承b和c
+# class A(B, C) :
+# 	xxx
